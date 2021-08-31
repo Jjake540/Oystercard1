8cd6dcb8abd3aa3910ac232eaaa4181b37f52751
Jordan <jordanveness8@gmail.com>
2021-08-31 16:58:06 +0100

    completed challenge 5: top up balance

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -5,4 +5,9 @@ class Oystercard
     @balance = 0
   end
 
+  def top_up(n)
+    @balance += n
+  end
+
+
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -4,7 +4,19 @@ describe Oystercard do
   it "should have a balance of 0" do
     expect(subject.balance).to eq(0)
   end
+  describe '#top_up' do
+    
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it "should top up balance" do 
+      expect{ subject.top_up 1 }.to change{ subject.balance }.by(1)
+    end
+
+  end
+
 end
+
+
 
 # NameError:
 #   uninitialized constant Oystercard
