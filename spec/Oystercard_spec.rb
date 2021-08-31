42630d45bba2b03ca159a42320160e007f6c2ad4
Jordan <jordanveness8@gmail.com>
2021-08-31 16:18:03 +0100

    completed challenge 4: add balance

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,3 +1,13 @@
-describe Oystercard do
+require './lib/Oystercard'
 
+describe Oystercard do
+  it "should have a balance of 0" do
+    expect(subject.balance).to eq(0)
+  end
 end
+
+# NameError:
+#   uninitialized constant Oystercard
+# ./spec/Oystercard_spec.rb.
+# ./spec/Oystercard_spec.rb:1:in `<top (required)>'
+# Name error: Raised when a given name is invalid or undefined.
